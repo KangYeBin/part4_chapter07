b866125850e1618b9da5fc8e68b783a019f86568
KangYeBin <yb725v@naver.com>
2022-06-20 00:29:43 +0900

    03. 사진 보여주기

--- app/src/main/java/com/yb/part4_chapter07/MainActivity.kt
+++ app/src/main/java/com/yb/part4_chapter07/MainActivity.kt
@@ -1,21 +1,33 @@
 package com.yb.part4_chapter07
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import androidx.core.content.getSystemService
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.yb.part4_chapter07.data.Repository
+import com.yb.part4_chapter07.databinding.ActivityMainBinding
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
     private val scope = MainScope()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
+        initViews()
+        bindViews()
         fetchRandomPhotos()
 
     }
@@ -25,9 +37,41 @@ class MainActivity : AppCompatActivity() {
         scope.cancel()
     }
 
+    private fun initViews() {
+        binding.recyclerView.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
+        binding.recyclerView.adapter = PhotoAdapter()
+    }
+
+    private fun bindViews() {
+        binding.searchEditText.setOnEditorActionListener { editText, actionId, event ->
+            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                currentFocus?.let { view ->
+                    val inputMethodManager =
+                        getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+                    inputMethodManager?.hideSoftInputFromWindow(view.windowToken, 0)
+
+                    view.clearFocus()
+
+                }
+
+                fetchRandomPhotos(editText.text.toString())
+            }
+            true
+        }
+
+        binding.refreshLayout.setOnRefreshListener {
+            fetchRandomPhotos(binding.searchEditText.text.toString())
+        }
+    }
+
     private fun fetchRandomPhotos(query: String? = null) = scope.launch {
         Repository.getRandomPhotos(query)?.let { photos ->
+            (binding.recyclerView.adapter as? PhotoAdapter)?.apply {
+                this.photos = photos
+                notifyDataSetChanged()
+            }
 
+            binding.refreshLayout.isRefreshing = false
 
         }
 
---
+++ app/src/main/java/com/yb/part4_chapter07/PhotoAdapter.kt
@@ -0,0 +1,72 @@
+package com.yb.part4_chapter07
+
+import android.graphics.drawable.Drawable
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.yb.part4_chapter07.data.models.PhotoResponse
+import com.yb.part4_chapter07.databinding.ItemPhotoBinding
+
+class PhotoAdapter : RecyclerView.Adapter<PhotoAdapter.ViewHolder>() {
+
+    var photos: List<PhotoResponse> = emptyList()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder =
+        ViewHolder(ItemPhotoBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(photos[position])
+    }
+
+    override fun getItemCount(): Int = photos.size
+
+
+    inner class ViewHolder(private val binding: ItemPhotoBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(photo: PhotoResponse) = with(binding) {
+            val dimensionRatio = photo.height / photo.width.toFloat()
+            val targetWidth = root.resources.displayMetrics.widthPixels -
+                    (root.paddingStart + root.paddingEnd)
+            val targetHeight = (targetWidth * dimensionRatio).toInt()
+
+            contentsContainer.layoutParams.height = targetHeight
+
+            Glide.with(root)
+                .load(photo.urls?.regular)
+                .thumbnail(
+                    Glide.with(root)
+                        .load(photo.urls?.thumb)
+                        .transition(DrawableTransitionOptions.withCrossFade())
+                )
+                .override(targetWidth, targetHeight)
+                .into(photoImageView)
+
+            Glide.with(root)
+                .load(photo.user?.profileImageUrls?.small)
+                .placeholder(R.drawable.shape_profile_placeholder)
+                .transition(DrawableTransitionOptions.withCrossFade())
+                .circleCrop()
+                .into(profileImageView)
+
+            if (photo.user?.name.isNullOrBlank()) {
+                authorTextView.visibility = View.GONE
+            } else {
+                authorTextView.visibility = View.VISIBLE
+                authorTextView.text = photo.user?.name
+            }
+
+            if (photo.description.isNullOrBlank()) {
+                descriptionTextView.visibility = View.GONE
+            } else {
+                descriptionTextView.visibility = View.VISIBLE
+                descriptionTextView.text = photo.description
+            }
+        }
+    }
+
+}
